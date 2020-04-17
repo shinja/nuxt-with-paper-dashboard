0f732c221504f176472928d25f2769fc8d8838bf
finley <FinleyLu@trevi.cc>
2020-04-17 13:59:21 +0800

    Add travis CI.

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ import { getRoutes } from './routes'
 
 export default {
   // routeNameSplitter: '/',
+  base: process.env.ROUTER_BASE || '',
   extendRoutes(routes, resolve) {
     const _routes = getRoutes(resolve)
     _routes.forEach((route) => {
